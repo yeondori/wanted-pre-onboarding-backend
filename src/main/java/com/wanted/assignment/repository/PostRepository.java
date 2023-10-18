2998b369d329cc855cb691d69fc69c3dc79b8bad
yeondori <yeonseo.jung0@gmail.com>
2023-10-18 23:44:27 +0900

    findBySearchKeyword 쿼리 수정

--- src/main/java/com/wanted/assignment/repository/PostRepository.java
+++ src/main/java/com/wanted/assignment/repository/PostRepository.java
@@ -10,12 +10,15 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<JobPosting, Long> {
 
+
     @Query("SELECT p FROM JobPosting p " +
             "WHERE p.company.name LIKE %:keyword% " +
             "OR p.company.country LIKE %:keyword% " +
             "OR p.company.region LIKE %:keyword% " +
             "OR p.jobPosition LIKE %:keyword% " +
-            "OR p.technologyUsed LIKE %:keyword%")
+            "OR p.recruitmentDetails LIKE %:keyword% " +
+            "OR p.technologyUsed LIKE %:keyword% " +
+            "OR CAST(p.recruitmentCompensation AS string) LIKE %:keyword%")
     List<JobPosting> findBySearchKeyword(String keyword);
 
     List<JobPosting> findByCompanyId(Long companyId);
