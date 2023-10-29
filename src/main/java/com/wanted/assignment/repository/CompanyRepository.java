7db648b13c7137408a8f738319ba4bacc47f7190
yeondori <yeonseo.jung0@gmail.com>
2023-10-29 22:43:10 +0900

    move: getJobPostingIdList 메서드 이동

--- src/main/java/com/wanted/assignment/repository/CompanyRepository.java
+++ src/main/java/com/wanted/assignment/repository/CompanyRepository.java
@@ -2,8 +2,15 @@ package com.wanted.assignment.repository;
 
 import com.wanted.assignment.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    @Query("SELECT p.id FROM JobPosting p where p.company.id = :companyId")
+    public List<Long> getJobPostingIdList(@Param("companyId") Long companyId);
 }
