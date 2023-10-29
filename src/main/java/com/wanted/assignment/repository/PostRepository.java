2a661ce964d496121982dc81b1a868c3c1c155af
yeondori <yeonseo.jung0@gmail.com>
2023-10-29 23:45:48 +0900

    chore:

--- src/main/java/com/wanted/assignment/repository/PostRepository.java
+++ src/main/java/com/wanted/assignment/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.wanted.assignment.repository;
 import com.wanted.assignment.domain.JobPosting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -19,7 +20,7 @@ public interface PostRepository extends JpaRepository<JobPosting, Long> {
             "OR p.details LIKE %:keyword% " +
             "OR p.techStack LIKE %:keyword% " +
             "OR CAST(p.compensation AS string) LIKE %:keyword%")
-    List<JobPosting> findBySearchKeyword(String keyword);
+    List<JobPosting> findBySearchKeyword(@Param("keyword") String keyword);
 
     List<JobPosting> findByCompanyId(Long companyId);
 }
--- src/main/java/com/wanted/assignment/service/ApplyStatus.java
+++ src/main/java/com/wanted/assignment/service/ApplyStatus.java
@@ -1,5 +1,8 @@
 package com.wanted.assignment.service;
 
 public enum ApplyStatus {
-    MEMBER_NOT_FOUND, POST_NOT_FOUND, ALREADY_APPLIED, SUCCESS
+    MEMBER_NOT_FOUND, POST_NOT_FOUND, ALREADY_APPLIED, SUCCESS;
+
+    ApplyStatus() {
+    }
 }
