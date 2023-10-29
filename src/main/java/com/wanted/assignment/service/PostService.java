9f93ae594a88dbd744402db1c0d18bf346216263
yeondori <yeonseo.jung0@gmail.com>
2023-10-29 22:53:24 +0900

    move: mapToJobPostingDTOList 메서드 이동

--- src/main/java/com/wanted/assignment/service/PostService.java
+++ src/main/java/com/wanted/assignment/service/PostService.java
@@ -2,12 +2,14 @@ package com.wanted.assignment.service;
 
 import com.wanted.assignment.domain.Company;
 import com.wanted.assignment.domain.JobPosting;
+import com.wanted.assignment.repository.CompanyRepository;
 import com.wanted.assignment.repository.PostRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
@@ -17,10 +19,12 @@ import java.util.Optional;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final CompanyRepository companyRepository;
 
     @Autowired
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, CompanyRepository companyRepository) {
         this.postRepository = postRepository;
+        this.companyRepository = companyRepository;
     }
 
     public void save(JobPosting jobPosting) {
@@ -58,4 +62,25 @@ public class PostService {
     }
 
     public List<JobPosting> findByCompanyId(Long companyId) {return postRepository.findByCompanyId(companyId);}
+
+    public List<Long> getJobPostingIdList(Long companyId) {return companyRepository.getJobPostingIdList(companyId);}
+
+    public List<JobPostingDTO> mapToJobPostingDTOList(List<JobPosting> jobPostings) {
+        List<JobPostingDTO> jobPostingDTOs = new ArrayList<>();
+
+        for (JobPosting jobPosting : jobPostings) {
+            JobPostingDTO dto = new JobPostingDTO();
+            dto.setId(jobPosting.getId());
+            dto.setCompanyName(jobPosting.getCompany().getName());
+            dto.setCompanyCountry(jobPosting.getCompany().getCountry());
+            dto.setCompanyRegion(jobPosting.getCompany().getRegion());
+            dto.setPosition(jobPosting.getPosition());
+            dto.setCompensation(jobPosting.getCompensation());
+            dto.setTechStack(jobPosting.getTechStack());
+
+            jobPostingDTOs.add(dto);
+        }
+
+        return jobPostingDTOs;
+    }
 }
